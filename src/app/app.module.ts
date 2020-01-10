0f461d310cdb25e249a6245a9683f4222acc6c81
mDavid <dev.mdavid@gmail.com>
2020-01-10 11:01:28 -0800

    provide youtubeInjectables in the appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { YoutubeInjectables } from './youtube-search/youtube-injectables';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [YoutubeInjectables],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
