27a446a2a0fbf6d0ad981d7e43235fba06f2e033
mDavid <dev.mdavid@gmail.com>
2020-01-10 10:59:47 -0800

    config youtube injectables

--- src/app/youtube-search/youtube-injectables.ts
+++ src/app/youtube-search/youtube-injectables.ts
@@ -1,2 +1,5 @@
-export class YoutubeInjectables {
-}
+export const YoutubeInjectables: Array<any> =  [
+  {provide: YouTubeSearchService , useClass: YouTubeSearchService},
+  {provide: YOUTUBE_API_KEY, useValue: YOUTUBE_API_KEY},
+  {provide: YOUTUBE_API_KEY, useValue: YOUTUBE_API_KEY}
+];
