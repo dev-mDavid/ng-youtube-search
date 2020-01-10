7ef09a29e1460454a79669529836e7de261ea4af
mDavid <dev.mdavid@gmail.com>
2020-01-10 10:51:09 -0800

    generate youtube injectables to create subsitution for testing APIs

---
+++ src/app/youtube-search/youtube-injectables.spec.ts
@@ -0,0 +1,7 @@
+import { YoutubeInjectables } from './youtube-injectables';
+
+describe('YoutubeInjectables', () => {
+  it('should create an instance', () => {
+    expect(new YoutubeInjectables()).toBeTruthy();
+  });
+});
