63918bd2e09fdf3031ac56fed73a910df35777d4
mDavid <dev.mdavid@gmail.com>
2020-01-10 10:25:42 -0800

    generate youtube search module and search result model

---
+++ src/app/youtube-search/search-result.model.spec.ts
@@ -0,0 +1,7 @@
+import { SearchResult.Model } from './search-result.model';
+
+describe('SearchResult.Model', () => {
+  it('should create an instance', () => {
+    expect(new SearchResult.Model()).toBeTruthy();
+  });
+});
